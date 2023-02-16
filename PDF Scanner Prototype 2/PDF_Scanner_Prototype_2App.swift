e26b596e24db1ca9df55ef9fdf868820d025fd12
Daniel Tvorun <danieltvorun@icloud.com>
2023-02-16 19:01:45 +0400

    Initial Commit

---
+++ PDF Scanner Prototype 2/PDF_Scanner_Prototype_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_Scanner_Prototype_2App.swift
+//  PDF Scanner Prototype 2
+//
+//  Created by Daniel Tvorun on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PDF_Scanner_Prototype_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
